6561eb84dc7618eac7e01214beef015c1122333a
vcardoso <vitor_cardoso@msn.com>
2017-05-16 18:03:42 +0100

    dev web page templates, controllers and some events

--- front-end/src/main/webapp/js/app.js
+++ front-end/src/main/webapp/js/app.js
@@ -8,24 +8,20 @@
 
     app.config(function ($routeProvider) {
         $routeProvider
-            .when("/", {
+            .when("/users", {
                 templateUrl: "templates/users.html",
                 controller: "UserController"
             })
-            .when("/add/user", {
-                templateUrl: "templates/addUser.html",
-                controller: "AddUserController"
-            })
-            .when("/add/song", {
-                templateUrl: "templates/addSong.html",
-                controller: "AddSongController"
+            .when("/users/:id/songs", {
+                templateUrl: "templates/favSongs.html",
+                controller: "FavSongController"
             })
             .when("/songs", {
                 templateUrl: "templates/songs.html",
                 controller: "SongController"
             })
             .otherwise({
-                redirectTo: "/"
+                redirectTo: "/users"
             });
     });
 
@@ -34,36 +30,78 @@
             HttpPendingRequestsService.cancelAll();
         };
         $scope.users = [{
+            "uuid": "123",
             "name": "ola",
             "email": "teste@teste.pt"
         }, {
+            "uuid": "1232",
             "name": "ola",
             "email": "teste@teste.pt"
         }];
-        $scope.goToAddUser = function () {
-            $location.path('/add/user');
+
+        $scope.isValid = function () {
+            return ($scope.name && $scope.email);
         };
-        $scope.goToSong = function () {
-            $location.path('/song');
+
+        $scope.insert = function () {
+            console.log("Insert User: " + $scope.name);
         };
-    }]);
-    app.controller('AddUserController', ['$http', '$scope', '$location', 'HttpPendingRequestsService', function ($http, $scope, $location, HttpPendingRequestsService) {
-        $scope.goBack = function () {
-            $location.path('/');
+
+        $scope.removeUser = function (uuid) {
+            console.log("remove User: " + uuid);
+            return false;
         };
-    }]);
-    app.controller('AddSongController', ['$http', '$scope', '$location', 'HttpPendingRequestsService', function ($http, $scope, $location, HttpPendingRequestsService) {
-        $scope.goBack = function () {
+
+        $scope.goToFavSongs = function (id) {
+            $location.path('/users/' + id + '/songs');
+        }
+        $scope.goToSongs = function () {
             $location.path('/songs');
         };
+        $scope.goToUsers = function () {
+            $location.path('/users');
+        };
     }]);
+
     app.controller('SongController', ['$http', '$scope', '$location', 'HttpPendingRequestsService', function ($http, $scope, $location, HttpPendingRequestsService) {
-        $scope.goBack = function () {
-            $location.path('/');
+        $scope.songs = [{
+            "uuid": "123",
+            "title": "ola",
+            "artist": "xxxxxxx",
+            "album": "oleole"
+        }, {
+            "uuid": "123s",
+            "title": "ola",
+            "artist": "xxxxxxx",
+            "album": "oleole"
+        }];
+
+        $scope.isValid = function () {
+            return ($scope.title && $scope.artist && $scope.album);
         };
-        $scope.goToAddSong = function () {
-            $location.path('/add/song');
+
+        $scope.insert = function () {
+            console.log("Insert song: " + $scope.title);
         };
+
+        $scope.removeSong = function (uuid) {
+            console.log("remove song: " + uuid);
+        };
+    }]);
+
+    app.controller('FavSongController', ['$http', '$scope', '$location', '$routeParams', 'HttpPendingRequestsService', function ($http, $scope, $location, $routeParams, HttpPendingRequestsService) {
+        $scope.params = $routeParams;
+
+        $scope.removeFav = function (userfavSongId) {
+            console.log("remove fav song: " + userfavSongId);
+        };
+
+        $scope.favSongs = [{
+            "uuid": "123",
+            "title": "ola",
+            "artist": "xxxxxxx",
+            "album": "oleole"
+        }];
     }]);
 
 })();
