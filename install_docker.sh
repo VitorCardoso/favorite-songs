299b331ec769ba535de5fdc23169416db5689e55
vcardoso <vcardoso@ubiwhere.com>
2017-07-25 10:51:13 +0100

    update version docker && docker-compose

--- install_docker.sh
+++ install_docker.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 echo "#############################################################"
-echo "Checking for Updates ..."
-echo "#############################################################"
-sudo apt-get update --yes
-echo "#############################################################"
 echo "Installing docker ..."
 echo "#############################################################"
-sudo apt-get install apt-transport-https ca-certificates --yes
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" > /etc/apt/sources.list.d/docker.list
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable"
 sudo apt-get update --yes
-sudo apt-get purge lxc-docker --yes
-sudo apt-get install docker-engine --yes
+sudo apt-get install docker-ce --yes
 sudo service docker start
-sudo curl -L "https://github.com/docker/compose/releases/download/1.13.0/docker-compose-$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.14.0/docker-compose-$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo docker --version
 sudo docker-compose --version
